781a6ca589992243de6a501e3b0836ac105d3e8e
Elizaveta1094 <treninskaya.elizaveta@yandex.ru>
2022-12-30 13:52:05 +0300

    hw9

---
+++ task 64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64
+// Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до 1.
+//Решить с помощью рекурсии.
+void Numbers (int num)
+{
+    if(num == 0)
+    {
+        return;
+    }
+    Console.WriteLine($"{num}"); // вывод в обратный порядок. Сначала выводит, потом уменьшает
+    Numbers(num - 1);
+}
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Numbers(a);
---
+++ task 66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66;
+// Задайте значение M и N. Напишитн программу, которая найдет сумму натуральных  
+// элементов в промежутке от M до N.
+
+int Stepen (int a, int b)
+{
+    if (a == b)
+    {
+        return a; 
+    }
+return a+Stepen(a+1, b);
+}
+Console.WriteLine("Введите первое число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Stepen(a,b));
